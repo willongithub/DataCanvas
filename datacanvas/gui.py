c1b15e94a6e63f4a243ecb730214dfc15f105fed
liam@macos <me@macos.com>
2022-05-30 00:52:48 +1000

    Refactor to widget class

--- datacanvas/gui.py
+++ datacanvas/gui.py
@@ -3,9 +3,12 @@
 """General Tkinter GUI."""
 
 import tkinter as tk
+from platform import system
 from tkinter import ttk
 from tkinter.messagebox import WARNING, askokcancel, showinfo
 from tkinter.scrolledtext import ScrolledText
+
+from PIL import Image, ImageTk
 
 import datacanvas.utils as util
 
@@ -17,36 +20,28 @@ PADDING = 10
 
 
 class DataCanvas(tk.Tk):
-    """GUI class."""
+    """GUI window class."""
 
     def __init__(self):
         super().__init__()
 
         self.title('DataCanvas')
         self.resizable(True, True)
-        self.iconbitmap('datacanvas/datacanvas.ico')
+        
+        platformD = system()
+        if platformD == 'Darwin':
+            icon = 'datacanvas/datacanvas.icns'
+        elif platformD == 'Windows':
+            icon = 'datacanvas/datacanvas.ico'
+        else:
+            icon = 'datacanvas/datacanvas.xbm'
+        self.iconbitmap(icon)
 
         # Set theme
         self.tk.call("source", "assets/theme/sun-valley.tcl")
         self.tk.call("set_theme", "light")
         # style = ttk.Style(self)
         # style.theme_use('clam')
-
-        self.field_1_option = tk.StringVar(self)
-        self.field_1_options = ('1', '3', '5', '7')
-        self.field_2_option = tk.StringVar(self)
-        self.field_2_options = ('0.5', '0.6', '0.7', '0.8', '0.9')
-
-        self.detection_mask = tk.BooleanVar(self)
-        self.face_gender = tk.BooleanVar(self)
-        self.face_ethnicity = tk.BooleanVar(self)
-        self.face_emotion = tk.BooleanVar(self)
-        self.face_age = tk.BooleanVar(self)
-        self.image_quality = tk.DoubleVar(self)
-        self.confidance_level = tk.DoubleVar(self)
-
-        self.image_quality_options = ('1', '3', '5', '7')
-        self.confidance_level = ('0.5', '0.6', '0.7', '0.8', '0.9')
 
         # Calculates location of screen centre to put the gui window.
         screen_width = self.winfo_screenwidth()
@@ -55,9 +50,40 @@ class DataCanvas(tk.Tk):
         center_y = int(screen_height/2 - HEIGHT/2)
         self.geometry(f'{WIDTH}x{HEIGHT}+{center_x}+{center_y}')
 
+        self.app = Page(self)
+        self.app.pack(fill="both", expand=True)
+
+
+class Page(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self)
+
+        # Make the app responsive
+        for index in [0, 1, 2]:
+            self.columnconfigure(index=index, weight=1)
+            self.rowconfigure(index=index, weight=1)
+
+        # Create option list
+        self.image_quality_options = ['1', '3', '5', '7']
+        self.confidance_level = ['0.5', '0.6', '0.7', '0.8', '0.9']
+
+        # Create option variables
+        self.detection_mask = tk.BooleanVar(value=True)
+        self.face_gender = tk.BooleanVar(value=True)
+        self.face_ethnicity = tk.BooleanVar(value=True)
+        self.face_emotion = tk.BooleanVar(value=True)
+        self.face_age = tk.BooleanVar(value=True)
+        self.image_quality = tk.DoubleVar(value=self.image_quality_options[1])
+        self.confidance_level = tk.DoubleVar(value=self.confidance_level[1])
+    
+        # Create widget
+        self.setup_widgets()
+
+    def setup_widgets(self):
         self.notebook = ttk.Notebook(self)
         self.notebook.pack(padx=PADDING, pady=PADDING, side='left')
 
+        # Shell style output area
         self.shell = ScrolledText(
             self,
             bg='gray20',
@@ -68,136 +94,89 @@ class DataCanvas(tk.Tk):
 
         self.canvas = tk.Canvas()
 
-        sidebar_options = {
+        # Tab
+        self.tab = Tab(self.notebook)
+        self.notebook.add(self.tab, text="Tab New")
+
+class Tab(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self)
+
+        self.sidebar_options = {
             'side': 'left', 'fill': 'both',
             'padx': PADDING, 'pady': PADDING}
 
-        # Tab 1
-        self.tab_1 = ttk.Frame(
-            self.notebook,
+        self.field_options = ('1', '3', '5', '7')
+
+        self.field_option = tk.StringVar(value=self.field_options[1])
+        
+        # Create widget
+        self.setup_widgets()
+
+    def setup_widgets(self):
+        self.tab = ttk.Frame(
+            self,
             width=WIDTH-SHELL,
             height=HEIGHT)
-        self.tab_1.pack(fill='both', expand=True)
-        self.notebook.add(self.tab_1, text='Tab 1')
+        self.tab.pack(fill='both', expand=True)
+        # self.add(self.tab, text='Tab 1')
         
-        self.sidebar_1 = ttk.LabelFrame(
-            self.tab_1,
+        self.sidebar = ttk.LabelFrame(
+            self.tab,
             text='OPTIONS',
         )
-        self.sidebar_1.pack(**sidebar_options)
+        self.sidebar.pack(**self.sidebar_options)
         
-        self.field_1 = ttk.LabelFrame(
-            self.sidebar_1,
+        self.field = ttk.LabelFrame(
+            self.sidebar,
             text='Field 1:',
         )
-        self.field_1.pack(padx=PADDING, pady=PADDING)
+        self.field.pack(padx=PADDING, pady=PADDING)
         ttk.OptionMenu(
-            self.field_1,
-            self.field_1_option,
-            self.field_1_options[0],
-            *self.field_1_options,
+            self.field,
+            self.field_option,
+            self.field_options[0],
+            *self.field_options,
         ).pack()
 
         # Plotting area.
-        self.plots_1 = ttk.Notebook(
-            self.tab_1,
+        self.plots = ttk.Notebook(
+            self.tab,
             width=WIDTH-SHELL,
             height=HEIGHT
         )
-        self.plots_1.pack(padx=PADDING, pady=PADDING, side='left')
+        self.plots.pack(padx=PADDING, pady=PADDING, side='left')
         self.tab_1_1 = ttk.Frame(
-            self.plots_1,
+            self.plots,
             width=WIDTH-SHELL,
             height=HEIGHT
         )
         self.tab_1_1.pack(side='left', fill='y')
-        self.plots_1.add(self.tab_1_1, text='tab_1_1')
+        self.plots.add(self.tab_1_1, text='tab_1_1')
         self.tab_1_2 = ttk.Frame(
-            self.plots_1,
+            self.plots,
             width=WIDTH-SHELL,
             height=HEIGHT
         )
         self.tab_1_2.pack(side='left', fill='y')
-        self.plots_1.add(self.tab_1_2, text='tab_1_2')
+        self.plots.add(self.tab_1_2, text='tab_1_2')
 
-        # Run and Exit buttons (Tab 1).
+        # Run and Exit buttons.
         ttk.Button(
-            self.sidebar_1,
+            self.sidebar,
             text='Exit',
             command=self._exit
         ).pack(padx=PADDING, pady=PADDING, side='bottom')
         ttk.Button(
-            self.sidebar_1,
+            self.sidebar,
             text='Clear',
             command=lambda: self._clear_shell()
         ).pack(padx=PADDING, pady=PADDING, side='bottom')
         ttk.Button(
-            self.sidebar_1,
+            self.sidebar,
             text='Theme',
             command=lambda: self._change_theme()
         ).pack(padx=PADDING, pady=PADDING, side='bottom')
-
-
-        # Tab 2
-        self.tab_2 = ttk.Frame(
-            self.notebook,
-            width=WIDTH-SHELL,
-            height=HEIGHT)
-        self.tab_2.pack(fill='both', expand=True)
-        self.notebook.add(self.tab_2, text='Tab 2')
-        
-        self.sidebar_2 = ttk.LabelFrame(
-            self.tab_2,
-            text='OPTIONS',
-        )
-        self.sidebar_2.pack(**sidebar_options)
-        
-        self.field_2 = ttk.LabelFrame(
-            self.sidebar_2,
-            text='Field 2:',
-        )
-        self.field_2.pack(padx=PADDING, pady=PADDING)
-        ttk.OptionMenu(
-            self.field_2,
-            self.field_2_option,
-            self.field_2_options[0],
-            *self.field_2_options,
-        ).pack()
-
-        # Plotting area.
-        self.plots_2 = ttk.Notebook(
-            self.tab_2,
-            width=WIDTH-SHELL,
-            height=HEIGHT
-        )
-        self.plots_2.pack(padx=PADDING, pady=PADDING, side='left')
-        self.tab_2_1 = ttk.Frame(
-            self.plots_2,
-            width=WIDTH-SHELL,
-            height=HEIGHT
-        )
-        self.tab_2_1.pack(side='left', fill='y')
-        self.plots_2.add(self.tab_2_1, text='tab_2_1')
-        self.tab_2_2 = ttk.Frame(
-            self.plots_2,
-            width=WIDTH-SHELL,
-            height=HEIGHT
-        )
-        self.tab_2_2.pack(side='left', fill='y')
-        self.plots_2.add(self.tab_2_2, text='tab_2_2')
-
-        # Run and Exit buttons (Tab 2).
-        ttk.Button(
-            self.sidebar_2,
-            text='Exit',
-            command=self._exit
-        ).pack(padx=PADDING, pady=PADDING, side='bottom')
-        ttk.Button(
-            self.sidebar_2,
-            text='Clear',
-            command=lambda: self._clear_shell()
-        ).pack(padx=PADDING, pady=PADDING, side='bottom')
-    
 
     # Local functions
     def _clear_shell(self):
@@ -229,3 +208,15 @@ class DataCanvas(tk.Tk):
         )
         if answer:
             exit()
+
+class Option(ttk.Frame):
+    pass
+
+class Shell(ttk.Frame):
+    pass
+
+class Canvas(ttk.Frame):
+    pass
+
+class ImageSelector(ttk.Frame):
+    pass
