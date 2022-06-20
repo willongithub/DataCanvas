294117837af670beefda0a04184c2f2e06f5a7e7
liam@macos <me@macos.com>
2022-06-21 00:13:27 +1000

    Update run script

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,4 @@
+### For Linux Host
 # xhost +
 
 # docker run --rm \
@@ -7,13 +8,24 @@
 
 # xhost -
 
-LOCAL=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}') 
 
-xhost + $LOCAL
+### For Darwin Host (bug not resolved)
+# DISPLAY=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}') 
+
+# xhost + DISPLAY
+
+# docker run --rm \
+#     -e DISPLAY=$DISPLAY:0 \
+#     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+#     datacanvas-3.10
+
+# xhost - DISPLAY
+
+
+### For Windows Host
+DISPLAY=$(ipconfig | grep IPv4 | awk '{print $NF}' | awk 'FNR <= 1') 
 
 docker run --rm \
-    -e DISPLAY=$LOCAL:0 \
+    -e DISPLAY=$DISPLAY:0 \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     datacanvas-3.10
-
-xhost - $LOCAL
